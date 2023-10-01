0c87e70e3abb5796dcd7b921c8b8f9abcdc9a4ae
zaaiiin <zaainnaseem@gmail.com>
2023-10-01 15:52:07 +0800

    add styles to hero title, button and paragraph

--- src/components/hero.js
+++ src/components/hero.js
@@ -9,14 +9,12 @@ const Hero = () => {
     <hero>
       <div className="hero flex-container">
         <div className="hero-content col-4 ">
-          <h1 className="hero-title bg-red  ">Hello, I'm Zaain </h1>
+          <h1 className="hero-title ">Hello, I'm Zaain </h1>
           <p className="hero-paragraph">
             I am a frontend developer and I love seeing ideas come to life on
             the screen! I also love learning new skills.
           </p>
-          <button className="more-btn bg-sky-800 text-white">
-            More about me
-          </button>
+          <button className="more-btn  ">More about me</button>
         </div>
 
         <div className="hero-photo col-2">
