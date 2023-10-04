2c1b6e2b7974a097f0fea127fdb10c29d5b37d64
zaaiiin <zaainnaseem@gmail.com>
2023-10-04 12:02:58 +0800

    add styles to project cards

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,29 +1,44 @@
 import React from "react";
 import { projects } from "./data";
 import "@/app/flexbox.css";
+import "@/app/globals.css";
+import Image from "next/image";
+import "@/styles/projects.css";
 
 const ProjectsCards = () => {
   return (
     <>
-      <div className="projects flex-container bg-primaryOrange text-white m-20">
+      <div className="projects flex-container  text-black mx-0 justify-center">
         {projects.map((value, index) => {
-          const { project, caption, skills } = value;
+          const { project, caption, skills, cover } = value;
 
           return (
-            <div
-              className="project-card col-2 border-[2px] w-[200px]"
-              key={index}
-            >
-              <h2 className="project-name">{project}</h2>
-              <div>
-                <p className="project-caption">{caption}</p>
-              </div>
+            <div className="projects-container justify-center">
+              <h2 className="project-name col-2 text-xl">{project}</h2>
 
-              <ul className="skills-used">
-                {skills.map((skill, skillIndex) => (
-                  <li key={skillIndex}>{skill}</li>
-                ))}
-              </ul>
+              <div
+                className="project-card shadow w-[300px] border-[2px] bg-primaryOrange"
+                key={index}
+              >
+                <div className="img h-40">
+                  <Image src={cover} alt="project-screenshot" />
+                </div>
+
+                <div>
+                  <p className="project-caption">{caption}</p>
+                </div>
+
+                <ul className="skills-used flex flex-row flex-wrap cursor-pointer">
+                  {skills.map((skill, skillIndex) => (
+                    <li
+                      className="each-skill bg-white px-2 m-2 "
+                      key={skillIndex}
+                    >
+                      {skill}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           );
         })}
