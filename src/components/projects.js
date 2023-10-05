3f66aeede3fb7f73faeb09d29483e8c06de45e09
zaaiiin <zaainnaseem@gmail.com>
2023-10-05 11:50:11 +0800

    add styles to project-cards

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,36 +8,40 @@ import "@/styles/projects.css";
 const ProjectsCards = () => {
   return (
     <>
-      <div className="projects flex-container  text-black mx-0 justify-center">
+      <div className="projects flex-container  text-black m-20 mx-0 justify-center gap-[30px]">
         {projects.map((value, index) => {
           const { project, caption, skills, cover } = value;
 
           return (
-            <div className="projects-container justify-center">
+            <div className="projects-container ">
               <h2 className="project-name col-2 text-xl">{project}</h2>
 
               <div
-                className="project-card shadow w-[300px] border-[2px] bg-primaryOrange"
+                className="project-card shadow w-[300px] h-[400px] border-[2px] bg-primaryOrange rounded-lg p-5 m-0 "
                 key={index}
               >
-                <div className="img h-40">
+                <div className="img h-40 justify-center">
                   <Image src={cover} alt="project-screenshot" />
                 </div>
 
-                <div>
-                  <p className="project-caption">{caption}</p>
+                <div className="project-caption_container flex-end flex">
+                  <p className="project-caption bg-black text-white p-4 mb-2 rounded-md ">
+                    {caption}
+                  </p>
                 </div>
 
-                <ul className="skills-used flex flex-row flex-wrap cursor-pointer">
-                  {skills.map((skill, skillIndex) => (
-                    <li
-                      className="each-skill bg-white px-2 m-2 "
-                      key={skillIndex}
-                    >
-                      {skill}
-                    </li>
-                  ))}
-                </ul>
+                <div className="skills-used_container">
+                  <ul className="skills-used flex flex-wrap justify-start cursor-pointer ">
+                    {skills.map((skill, skillIndex) => (
+                      <li
+                        className="each-skill bg-white px-3 m-1 rounded"
+                        key={skillIndex}
+                      >
+                        {skill}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
           );
