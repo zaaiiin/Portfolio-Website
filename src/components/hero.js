47a60160f7c91c15c9acc1a30e01e9089311d079
zaaiiin <zaainnaseem@gmail.com>
2023-10-13 17:21:50 +0800

    edit margins of the hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,7 +7,7 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <hero>
-      <div className="hero flex-container m-[25px] lg:m-[50px]">
+      <div className="hero flex-container m-[25px] lg:m-[50px] ">
         <div className="hero-content col-4 pt-[30px] pr-[50px] pb-[0px] pl-[50px]  xl:pl-[0px]">
           <h1 className="hero-title text-3xl lg:text-4xl xl:text-5xl">
             Hello, I'm Zaain{" "}
